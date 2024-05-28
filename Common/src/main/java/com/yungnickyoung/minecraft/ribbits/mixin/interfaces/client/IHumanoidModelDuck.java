0e363809b692140365f5fdf299ca444cd6260907
Nick Young <yungnickyoung@gmail.com>
2024-05-27 20:04:11 -0400

    Remove unused Maraca mixins.

--- Common/src/main/java/com/yungnickyoung/minecraft/ribbits/mixin/interfaces/client/IHumanoidModelDuck.java
+++
@@ -1,9 +0,0 @@
-package com.yungnickyoung.minecraft.ribbits.mixin.interfaces.client;
-
-public interface IHumanoidModelDuck {
-    boolean ribbits$isRightHandUsingMaraca();
-    boolean ribbits$isLeftHandUsingMaraca();
-
-    void ribbits$setRightHandUsingMaraca(boolean usingMaraca);
-    void ribbits$setLeftHandUsingMaraca(boolean usingMaraca);
-}
